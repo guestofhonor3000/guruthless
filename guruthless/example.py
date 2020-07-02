6ce9084df68454570e40db07152e44a41d76c075
guestofhonor3000 <56638053+guestofhonor3000@users.noreply.github.com>
2020-07-02 16:40:24 +0100

    Add files via upload

---
+++ guruthless/example.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from instabot_py import InstaBot
+
+bot = InstaBot(
+    login="guruthless",
+    password="Blahblahs**",
+    like_per_day=0,   
+    tag_list=["landscape"],
+    follow_per_day=0,
+    follow_time=1 * 20 * 20,
+    comments_per_day=0,
+    unfollow_per_day=1000,
+    unfollow_recent_feed=True,
+    log_mod=0,
+    proxy="",
+    # List of list of words, each of which will be used to generate comment
+    # For example: "This shot feels wow!"
+    comment_list=[
+        [
+            "great",
+            "super",
+            "good",
+            "very good",
+            "good",
+            "cool",
+            "magnificent",
+            "lovely",
+            "excellent",
+            "amazing",
+        ],
+        ["photo", "picture", "pic", "shot"],
+    ],
+    # Use unwanted_username_list to block usernames containing a string
+    # Will do partial matches; i.e. 'mozart' will block 'legend_mozart'
+    # 'free_followers' will be blocked because it contains 'free'
+    unwanted_username_list=[
+        "second",
+        "stuff",
+        "art",
+        "project",
+        "love",
+        "life",
+        "food",
+        "blog",
+        "free",
+        "keren",
+        "photo",
+        "graphy",
+        "indo",
+        "travel",
+        "art",
+        "shop",
+        "store",
+        "sex",
+        "toko",
+        "jual",
+        "online",
+        "murah",
+        "jam",
+        "kaos",
+        "case",
+        "baju",
+        "fashion",
+        "corp",
+        "tas",
+        "butik",
+        "grosir",
+        "karpet",
+        "sosis",
+        "salon",
+        "skin",
+        "care",
+        "cloth",
+        "tech",
+        "rental",
+        "kamera",
+        "beauty",
+        "express",
+        "kredit",
+        "collection",
+        "impor",
+        "preloved",
+        "follow",
+        "follower",
+        "gain",
+        ".id",
+        "_id",
+        "bags",
+    ],
+    unfollow_whitelist=["example_user_1", "example_user_2"],
+    # Enable the following to schedule the bot. Uses 24H
+    # end_at_h = 23, # Hour you want the bot to stop
+    # end_at_m = 30, # Minute you want the bot stop, in this example 23:30
+    # start_at_h = 9, # Hour you want the bot to start
+    # start_at_m = 10, # Minute you want the bot to start, in this example 9:10 (am).
+)
+
+bot.mainloop()
